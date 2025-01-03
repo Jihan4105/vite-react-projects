1ab9636da49f40b1308c491bc0f1929b5d96e6ef
Jihan4105 <wlgks6979@naver.com>
2025-01-03 21:59:52 +0900

    2025-01-03 Developments

---
+++ hans-universe-login/src/components/LoginContents.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react"
+import InputField from "./InputField.jsx"
+import { getUser } from "../services/fetchData.js"
+
+export default function LoginContents() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [whichIsWrong, setWhichIsWrong] = useState("")
+
+  return(
+    <>
+      <h1 className="login-title">Login</h1>
+      <p className="email-label">Email</p>
+      <InputField 
+        type="email"
+        state={email}
+        setState={setEmail}
+        placeholder="Email"
+        iconName="mail-outline"
+      />
+      <p 
+        className={
+          `error-text 
+          email-error 
+          ${whichIsWrong === "email-wrong" ? "show" : "invisible"}`}
+      >
+        no such user exsit
+      </p>
+      <p className="password-label">Password <span className="login-forgot">Forgot password?</span></p>
+      <InputField
+        type="password" 
+        state={password}
+        setState={setPassword}
+        placeholder="PWD"
+        iconName="lock-closed-outline"
+      /> 
+      <p 
+        className={
+          `error-text 
+          password-error
+          ${whichIsWrong === "password-wrong" ? "show" : "invisible"}`}
+      >
+        your password is wrong
+      </p>
+      <button className="login-btn" onClick={(e) => loginBtnClicked(e, email, password, setWhichIsWrong)}>Log in</button>
+      <p className="orsignup-text">Or Sign Up Using</p>
+      <div className="google-sign-up">
+        <ion-icon className="google-icon" name="logo-google"></ion-icon>
+      </div>
+      <p className="orsignup-text">Or Sign Up Using</p>
+      <span className="sign-up">SIGN UP</span>
+    </>
+  )
+}
+
+function loginBtnClicked(e, email, password, setState) {
+  e.preventDefault()
+
+  const userdata = getUser(email, password)
+
+  if(!userdata) { setState("email-wrong") } 
+  else if(userdata.password != password) { setState("password-wrong")}
+  else {console.log("Login Success!")}
+}
--- hans-universe-login/src/pages/IndexApp.jsx
+++ hans-universe-login/src/pages/IndexApp.jsx
@@ -1,64 +1,29 @@
 import { useState } from "react"
-import InputField from "../components/InputField.jsx"
-import { getUser } from "../services/fetchData.js"
+import LoginContents from "../components/LoginContents"
 
 export default function IndexApp() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [whichIsWrong, setWhichIsWrong] = useState("")
+  const [contentType, setContentType] = useState("login")
+  let content
 
+  // debugger
+  switch(contentType) {
+    case "login" :
+      content = <LoginContents />
+      break;
+    case "signup" :
+      content = "signup"
+      break;
+    case "forgot" :
+      content = "forgot"
+      break;
+    case "reset" :
+      content = "reset"
+      break;
+  }
+  
   return (
-    <form className="login-form">
-      <h1 className="login-title">Login</h1>
-      <p className="email-label">Email</p>
-      <InputField 
-        type="email"
-        state={email}
-        setState={setEmail}
-        placeholder="Email"
-        iconName="mail-outline"
-      />
-      <p 
-        className={
-          `error-text 
-          email-error 
-          ${whichIsWrong === "email-wrong" ? "show" : "invisible"}`}
-      >
-        no such user exsit
-      </p>
-      <p className="password-label">Password <span className="login-forgot">Forgot password?</span></p>
-      <InputField
-        type="password" 
-        state={password}
-        setState={setPassword}
-        placeholder="PWD"
-        iconName="lock-closed-outline"
-      /> 
-      <p 
-        className={
-          `error-text 
-          password-error
-          ${whichIsWrong === "password-wrong" ? "show" : "invisible"}`}
-      >
-        your password is wrong
-      </p>
-      <button className="login-btn" onClick={(e) => loginBtnClicked(e, email, password, setWhichIsWrong)}>Log in</button>
-      <p className="orsignup-text">Or Sign Up Using</p>
-      <div className="google-sign-up">
-        <ion-icon className="google-icon" name="logo-google"></ion-icon>
-      </div>
-      <p className="orsignup-text">Or Sign Up Using</p>
-      <span className="sign-up">SIGN UP</span>
+    <form className={`login-form ${contentType}`}>
+      {content}
     </form>
   )
-}
-
-function loginBtnClicked(e, email, password, setState) {
-  e.preventDefault()
-
-  const userdata = getUser(email, password)
-
-  if(!userdata) { setState("email-wrong") } 
-  else if(userdata.password != password) { setState("password-wrong")}
-  else {console.log("Login Success!")}
 }
