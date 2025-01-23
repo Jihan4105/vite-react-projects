041cbb2033d86fac7df0b55e7a9b6934f3789a53
Jihan4105 <wlgks6979@naver.com>
2025-01-23 17:57:03 +0900

    2025-01-23 Developments

--- hans-universe/src/components/landing/Hero.jsx
+++ hans-universe/src/components/landing/Hero.jsx
@@ -1,53 +1,49 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 
 import Planet from "./Planet"
 import astronautImg from "@assets/landing/astronaut.png"
 import profilePlanetImg from "@assets/landing/planet.png"
-import useWindow from "@/hooks/useWindow"
-
+import { JSXDispatchContext } from "@/contexts/JSXDispatchContext"
+import { WindowContext } from "@/contexts/WindowContext"
 
 export default function Hero() {
-  const [windowSize, setWindowSize] = useState({
-    innerWidth: window.innerWidth,
-    innerHeight: window.innerHeight,
-  })
-  
-  useWindow(windowSize, setWindowSize)
+  const windowWidth = useContext(WindowContext)
+  const JSXdispach = useContext(JSXDispatchContext)
 
   return (
     <section id="hero">
       <div className="container-jh">
         <div className="planet-box">
           <img className="profile-planet" src={profilePlanetImg} alt="planet" />
-          <a className="profile" title="Clck ME!"></a>
+          <a className="profile" title="Clck ME!" onClick={() => {JSXdispach({ docType: "profile" })}}></a>
         </div>
         <div className="astronaut-box">
           <h1 className="section-title">What Makes Me</h1>
           <div className="astronaut">
             <img className="astronaut-img gravity-moving" src={astronautImg} alt="" />
 
-            {windowSize.innerWidth > 450 &&
+            {windowWidth > 450 &&
             <>
               <Planet 
-                windowSize={windowSize}
+                windowWidth={windowWidth}
                 planetName="workout"
                 coords={["87,84,80", "76,74,71", "50,48,47"]}
               />
 
               <Planet 
-                windowSize={windowSize}
+                windowWidth={windowWidth}
                 planetName="books"
                 coords={["88,80,76", "75,73,72", "50,48,48"]}
               />
 
               <Planet 
-                windowSize={windowSize}
+                windowWidth={windowWidth}
                 planetName="skills"
                 coords={["87,85,79", "76,74,71", "50,48,47"]}
               />
 
               <Planet 
-                windowSize={windowSize}
+                windowWidth={windowWidth}
                 planetName="thoughts"
                 coords={["85,96,75", "76,85,66", "50,55,44"]}
               />
--- hans-universe/src/components/landing/Planet.jsx
+++ hans-universe/src/components/landing/Planet.jsx
@@ -11,12 +11,12 @@ import { JSXDispatchContext } from "@contexts/JSXDispatchContext"
 import { initPageScroll } from "@utils/utils"
 
 Planet.propTypes = {
-  windowSize: PropTypes.object.isRequired,
+  windowWidth: PropTypes.number.isRequired,
   planetName: PropTypes.string.isRequired,
   coords: PropTypes.array.isRequired
 }
 
-export default function Planet({ windowSize, planetName, coords }) {
+export default function Planet({ windowWidth, planetName, coords }) {
   const JSXdispatch = useContext(JSXDispatchContext)
 
   let planetImg
@@ -38,19 +38,19 @@ export default function Planet({ windowSize, planetName, coords }) {
   return(
     <>
       <img className={`${planetName}-planet`} src={planetImg} useMap={`#${planetName}-planet`} />
-      {windowSize.innerWidth > 768
+      {windowWidth > 768
         &&    
         <map name={`${planetName}-planet`}>
           <area shape="circle" coords={coords[0]} onClick={() => {JSXdispatch({ docType: planetName }); initPageScroll();}} title={`${planetName}`}  style={{ cursor: "pointer" }}/>
         </map>
       }
-      {windowSize.innerWidth <= 768 && windowSize.innerWidth > 576
+      {windowWidth <= 768 && windowWidth > 576
         && 
         <map name={`${planetName}-planet`}>
           <area shape="circle" coords={coords[1]} onClick={() => {JSXdispatch({ docType: planetName }); initPageScroll();}} title={`${planetName}`} style={{ cursor: "pointer" }}/>
         </map>
       }
-      {windowSize.innerWidth <= 576 && 
+      {windowWidth <= 576 && 
         <map name={`${planetName}-planet`}>
           <area shape="circle" coords={coords[2]} onClick={() => {JSXdispatch({ docType: planetName }); initPageScroll();}} title={`${planetName}`} style={{ cursor: "pointer" }}/>
         </map>
